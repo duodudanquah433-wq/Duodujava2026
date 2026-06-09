d3ada349d90c6da5e2d13d06af8b1f7c6429cf2c
duodudanquah433-wq <duodudanquah433@gmail.com>
2026-06-09 14:06:11 +0000

    Add files via upload

---
+++ Hellojava2.java
@@ -0,0 +1,13 @@
+public class Hellojava2 {
+    public static void main(String[] args){
+        System.out.println("x                                                 x");
+        System.out.println("x                                                 x");
+        System.out.println("x                                                 x");
+        System.out.println("x                                                 x");
+        System.out.println("x x x x x x x x    x x x x x x x x    x x x x x x x");
+        System.out.println("x             x    x             x    x            x");
+        System.out.println("x             x    x             x    x            x");
+        System.out.println("x             x    x             x    x            x");
+        System.out.println("x             x    x             x    x            x");
+    }
+}
